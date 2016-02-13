aad7eb69f3e6adc518ea521f45b8edc532687a0d
moqada <moqada@gmail.com>
2016-02-13 19:59:34 +0900

    docs(ESDoc): Fix Doc comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import Dacho from './Dacho';
 
 /**
+ * @typedef {Function} Reaction
+ * @param {string[]|Object} keys object keys
+ * @param {string} [prefix=''] prefix of value (Optional)
+ * @return {Object}
+ * @example
+ * reaction({ACTION1: null, ACTION2: null}, 'FOO/');
+ * // {ACTION1: 'FOO/ACTION1', ACTION2: 'FOO/ACTION2'};
+ *
+ * reaction(['ACTION1', 'ACTION2'], 'BAR/');
+ * // {ACTION1: 'BAR/ACTION1', ACTION2: 'BAR/ACTION2'};
+ */
+
+/**
  * createReaction
  *
- * @param {string} [globalPrefix] prefix for every key
- * @return {Function}
+ * @param {string} [globalPrefix=''] prefix for every key
+ * @return {Reaction}
  */
 function createReaction(globalPrefix = '') {
   const dacho = new Dacho(globalPrefix);
@@ -13,6 +26,11 @@ function createReaction(globalPrefix = '') {
   };
 }
 
+/**
+ * reaction
+ *
+ * @type {Reaction}
+ */
 const reaction = createReaction();
 
 export {Dacho, createReaction, reaction};
